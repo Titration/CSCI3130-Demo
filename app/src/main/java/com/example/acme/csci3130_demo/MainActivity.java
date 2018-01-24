106ba507c00f2b5c3c5edcc4a51e8019ea9194a6
sxue <sampsonxue@gmail.com>
2018-01-24 02:49:41 -0400

    first commit

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //declare EditText, TextView and Button by each id
+        final EditText edit = (EditText) findViewById(R.id.editText_1);
+        final TextView text = (TextView) findViewById((R.id.textView1));
+        Button button = (Button) findViewById(R.id.btn1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              String name = edit.getText().toString();
+              text.setText(name);
+            }
+        });
     }
 }
+
